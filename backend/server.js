04ba70241322e85fd4992d6da7993afad77936a6
Eli <elikembuertey@gmail.com>
2024-08-31 09:13:53 -0400

    Testing connection to the mongodb URI

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
-const express = require("express");
+import express from "express";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {res.send("Hello world")})
-app.listen(5000, ()=>console.log("Server running on port 5000"));
+// Test MongoDB connection
+connectToMongoDB();
+
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
