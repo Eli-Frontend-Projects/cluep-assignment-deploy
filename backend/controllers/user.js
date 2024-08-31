6296675c183c8e91bcc76956a55e50550dfdc41e
Eli <elikembuertey@gmail.com>
2024-08-31 12:15:22 -0400

    added automatic id generation

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,10 @@
 import bcrypt from 'bcrypt';
+import { v4 as uuidv4 } from 'uuid';
 import { User } from '../db/models/user.js';
 
-export async function createUser({ userID, firstName, lastName, email, password }) {
+export async function createUser({ firstName, lastName, email, password }) {
+  const userID = uuidv4();
+
   const hashedPassword = await bcrypt.hash(password, 10);
   
   const user = new User({
