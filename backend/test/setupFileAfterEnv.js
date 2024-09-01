a7b15bc837cb1fd0b865c298d058cf18fdbdc6e8
Eli <elikembuertey@gmail.com>
2024-08-31 21:13:36 -0400

    emptied the database after each test

--- backend/test/setupFileAfterEnv.js
+++ backend/test/setupFileAfterEnv.js
@@ -1,7 +1,11 @@
-import mongoose from 'mongoose'
-import { beforeAll, afterAll } from '@jest/globals'
+import mongoose from 'mongoose';
+import { beforeAll, afterAll, beforeEach } from '@jest/globals';
+import { User } from '../db/models/user.js';
+import { initDatabase } from '../db/init.js';
 
-import { initDatabase } from '../db/init.js'
+beforeEach(async () => {
+  await User.deleteMany({});
+})
 
 beforeAll(async () => {
   await initDatabase()
