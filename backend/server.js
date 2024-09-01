e37c74d3bbd682908cc07599725c9940aed953e0
Eli <elikembuertey@gmail.com>
2024-08-31 21:08:00 -0400

    Added routes to the server

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 import express from "express";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import dotenv from "dotenv";
+import cors from 'cors';
+import bodyParser from 'body-parser'
+
+import { userRoutes } from './routes/user.js'
 
 // Load environment variables from .env file
 dotenv.config();
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+userRoutes(app);
 
 // Test MongoDB connection
 connectToMongoDB();
