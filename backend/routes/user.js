891fa48623bc0d98f0d6e649ebb07e41ab39785e
Eli <elikembuertey@gmail.com>
2024-09-01 10:21:34 -0400

    sends token when loggedin

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,6 @@
 import {loginUser} from '../controllers/user.js'
 
 export function userRoutes(app) {
-  console.log("using this route")
   app.post('/api/login', async (req, res) => {
     try {
       const token = await loginUser(req.body)
