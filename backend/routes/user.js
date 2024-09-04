bac1e659db8917d7906a1f546ac2cf051579a476
Elikem Buertey <ebuertey@uwaterloo.ca>
2024-09-04 14:38:56 -0400

    Added sign up route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,4 @@
-import {loginUser} from '../controllers/user.js'
+import {loginUser, createUser} from '../controllers/user.js'
 
 export function userRoutes(app) {
   app.post('/api/login', async (req, res) => {
@@ -11,4 +11,17 @@ export function userRoutes(app) {
       })
     }
   })
+
+  app.post('/api/signup', async (req, res) => {
+    try {
+      const { firstName, lastName, email, password } = req.body;
+      console.log(req.body)
+      const user = await createUser({ firstName, lastName, email, password });
+      return res.status(201).send({ message: 'User created successfully', user });
+    } catch (err) {
+      return res.status(400).send({
+        error: 'Signup failed, could not create user.',
+      });
+    }
+  });
 }
