c4267ec8277a7c7a250caf29e489448ed42d239b
Elikem Buertey <ebuertey@uwaterloo.ca>
2024-09-04 07:18:42 -0400

    made the home page responsive

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Sidebar from './components/sidebar/Sidebar';
+import ChatArea from './components/chat/ChatArea';
 
 function App() {
   return (
-    <div className="flex">
-      <Sidebar />
+    <div className="flex flex-col md:flex-row h-screen">
+      {/* Sidebar */}
+      <div className="hidden md:flex md:w-64 bg-gray-200 p-4">
+        <Sidebar />
+      </div>
+
+      {/* Main Content Area */}
+      <div className="flex-1 flex flex-col">
+        <ChatArea />
+      </div>
     </div>
   );
 }
