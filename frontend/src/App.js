c7b8d4960c0d02595ce08ae9197c79aaf48e676e
Elikem Buertey <ebuertey@uwaterloo.ca>
2024-09-06 13:12:02 -0400

    added sidebar context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { SidebarProvider } from './components/sidebar/SidebarContext'; // Adjust path as needed
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Home from './pages/Home';
@@ -7,11 +8,13 @@ import Home from './pages/Home';
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
+      <SidebarProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </SidebarProvider>
     </Router>
   );
 }
