3ce01498d917be0604626ad7c99db12fea7f672a
Elikem Buertey <ebuertey@uwaterloo.ca>
2024-09-07 16:59:57 -0400

    refactored search button into a separate component

--- frontend/src/components/chat/Conversation.jsx
+++ frontend/src/components/chat/Conversation.jsx
@@ -4,7 +4,7 @@ import '../../css/Conversation.css'; // Import the CSS file from the src folder
 
 const Conversation = ({ conversations }) => {
   return (
-    <div className="flex flex-col space-y-2">
+    <div className="flex flex-col space-y-2 mt-3 ml-2 mb-4 overflow-y-auto overflow-x-hidden h-full">
       {conversations.map((msg, index) => (
         <div
           key={index}
---
+++ frontend/src/components/search/SearchButton.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const SearchButton = ({ onClick }) => {
+  return (
+    <div className="flex flex-col justify-between mt-2">
+      <div className=".btn flex justify-center">
+        <button onClick={onClick} className="hover:opacity-75">
+          <img
+            className="w-7 h-7 cursor-pointer"
+            src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
+            alt="search icon"
+          />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchButton;
