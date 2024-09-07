0c2704ef4c18053473c418b467d265199228bf31
Elikem Buertey <ebuertey@uwaterloo.ca>
2024-09-07 10:01:31 -0400

    make text and border black on hover

--- frontend/src/components/chat/TaskPanel.jsx
+++ frontend/src/components/chat/TaskPanel.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 // Function to create a button with hover effect
 const TaskButton = () => (
   <button
-    className="text-gray-500 rounded-2xl border border-gray-300 w-[149px] h-[40px] transform transition-transform duration-300 hover:-translate-y-1"
+    className="text-gray-500 border-gray-300 bg-transparent rounded-2xl border w-[149px] h-[40px] transform transition-transform duration-300 hover:-translate-y-1 hover:text-black hover:border-black"
   >
     Task
   </button>
@@ -11,7 +11,7 @@ const TaskButton = () => (
 
 const TaskPanel = () => {
   return (
-    <div className="flex justify-start  pt-3 col-span-2 scrollbar-hidden">
+    <div className="flex justify-start pt-3 col-span-2 scrollbar-hidden">
       {/* Container div for buttons */}
       <div className="flex space-x-2">
         <TaskButton />
