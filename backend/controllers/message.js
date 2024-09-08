bc4511d4a74286747473033aa6537d33bb00b31e
Elikem Buertey <ebuertey@uwaterloo.ca>
2024-09-08 11:30:09 -0400

    use projection to retrieve only the message from the database

--- backend/controllers/message.js
+++ backend/controllers/message.js
@@ -1,11 +1,18 @@
 import { Message } from '../db/models/message.js';
 
 export async function createMessage(userId, { message }) {
-  const newMessage = new Message({ userID: userId, message });
-
-  return await newMessage.save();
+  try {
+    const newMessage = new Message({ userID: userId, message });
+    return await newMessage.save();
+  } catch (err) {
+    throw new Error('Failed to create message');
+  }
 }
 
 export async function getMessages(userId) {
-  return await Message.find({ userID: userId }).sort({ createdAt: 'ascending' });
+  try {
+    return await Message.find({ userID: userId }, 'message').sort({ createdAt: 'ascending' });
+  } catch (err) {
+    throw new Error('Failed to retrieve messages');
+  }
 }
