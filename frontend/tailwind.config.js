4792c56920c7cbb6b5cd6f8c2678b70f170e366f
Elikem Buertey <ebuertey@uwaterloo.ca>
2024-09-08 15:34:57 -0400

    make basier circle regular default font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  content: ["./src/**/*.{jsx,js}"],
+  content: [
+    "./src/**/*.{jsx,js,ts,tsx}", // Include TypeScript files if you're using them
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        basier: ['BasierCircle', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
