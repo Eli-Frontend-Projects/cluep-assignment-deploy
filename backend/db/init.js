ad0012b633eda168e06885aed8a6f63f303e2da4
Elikem Buertey <ebuertey@uwaterloo.ca>
2024-09-08 15:34:28 -0400

    prevent the mongodb url from being printed

--- backend/db/init.js
+++ backend/db/init.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export function initDatabase() {
   const DATABASE_URL = process.env.MONGO_DB_URI;
   mongoose.connection.on('open', () => {
-    console.info('successfully connected to database:', DATABASE_URL)
+    console.info('successfully connected to database:')
   })
   const connection = mongoose.connect(DATABASE_URL)
   return connection
