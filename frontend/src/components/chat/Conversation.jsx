7bb940e31133c2c6d5abbc95ecce884522a71d2f
Elikem Buertey <ebuertey@uwaterloo.ca>
2024-09-08 11:42:08 -0400

    Add auto-scrolling feature to Conversation component.

--- frontend/src/components/chat/Conversation.jsx
+++ frontend/src/components/chat/Conversation.jsx
@@ -1,8 +1,14 @@
 // Conversation.js
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import '../../css/Conversation.css'; // Import the CSS file from the src folder
 
 const Conversation = ({ conversations }) => {
+  const endOfMessagesRef = useRef(null);
+
+  useEffect(() => {
+    endOfMessagesRef.current?.scrollIntoView({ behavior: 'auto' });
+  }, [conversations]);
+
   return (
     <div className="flex flex-col space-y-2 mt-3 ml-2 mb-4 overflow-y-auto overflow-x-hidden h-full">
       {conversations.map((msg, index) => (
@@ -13,6 +19,7 @@ const Conversation = ({ conversations }) => {
           {msg}
         </div>
       ))}
+      <div ref={endOfMessagesRef} /> {/* This div will act as the scroll anchor */}
     </div>
   );
 };
