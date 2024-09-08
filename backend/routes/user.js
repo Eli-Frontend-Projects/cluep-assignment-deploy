b01d115cacfa511d926d55d624570b900ac7dff1
Elikem Buertey <ebuertey@uwaterloo.ca>
2024-09-08 11:42:08 -0400

    Refactor user routes for consistency with new endpoints for login, signup, and user information.

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,27 +1,38 @@
-import {loginUser, createUser} from '../controllers/user.js'
+import { loginUser, createUser, getUserFullNameById } from '../controllers/user.js';
 
 export function userRoutes(app) {
-  app.post('/api/login', async (req, res) => {
+  app.post('/user/login', async (req, res) => {
     try {
-      const token = await loginUser(req.body)
-      return res.status(200).send({ token })
+      const token = await loginUser(req.body);
+      return res.status(200).send({ token });
     } catch (err) {
       return res.status(400).send({
         error: 'login failed, did you enter the correct username/password?',
-      })
+      });
     }
-  })
+  });
 
-  app.post('/api/signup', async (req, res) => {
+  app.post('/user/signup', async (req, res) => {
     try {
       const { firstName, lastName, email, password } = req.body;
-      console.log(req.body)
       const user = await createUser({ firstName, lastName, email, password });
       return res.status(201).send({ message: 'User created successfully', user });
     } catch (err) {
       return res.status(400).send({
         error: 'Signup failed, could not create user.',
       });
+    }
+  });
+
+  app.get('/user/:id', async (req, res) => {
+    try {
+      const userInfo = await getUserFullNameById(req.params.id);
+      if (!userInfo || !userInfo.username) {
+        return res.status(404).send({ error: 'User not found' });
+      }
+      return res.status(200).send(userInfo);
+    } catch (err) {
+      return res.status(500).send({ error: 'An error occurred while fetching user information' });
     }
   });
 }
