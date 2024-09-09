d51fa4cbdc0b2798098c04ebb81e3a0224078e1a
Elikem Buertey <ebuertey@uwaterloo.ca>
2024-09-09 08:33:30 -0400

    let each test should handle resetting its db env

--- backend/test/setupFileAfterEnv.js
+++ backend/test/setupFileAfterEnv.js
@@ -3,9 +3,6 @@ import { beforeAll, afterAll, beforeEach } from '@jest/globals';
 import { User } from '../db/models/user.js';
 import { initDatabase } from '../db/init.js';
 
-beforeEach(async () => {
-  await User.deleteMany({});
-})
 
 beforeAll(async () => {
   await initDatabase()
